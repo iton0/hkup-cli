3f9c155346be52ddcf7c0f63fd4ec4f807500e94
iton0 <iton442@gmail.com>
2024-11-19 22:26:06 -0500

    refactor(logic): update flags and simplify hook commands
    - Rename flags: `Lang` to `LangFlg`, `GitDir` to `GitDirFlg`, `WorkTree` to `WorkTreeFlg` for consistency.
    - Simplify the Add, Remove, and Init logic by centralizing directory checks and reducing redundant code.
    - Update file handling to improve readability and error handling.
    - Adjust comments for better clarity and consistency across functions.

--- internal/logic/list.go
+++ internal/logic/list.go
@@ -6,24 +6,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// List displays a list of available Git hooks or supported languages based on the provided argument.
-// It takes a single argument, which determines whether to list hooks or languages.
+// List displays a list of available Git hooks or supported languages based on
+// the provided argument.
 //
-// Returns:
-// - error: Returns an error if the argument is invalid; otherwise, it returns nil.
+// Returns error if the argument is invalid.
 func List(cmd *cobra.Command, args []string) error {
 	arg := args[0]
-	var output []string
+	out := []string{}
 
-	// NOTE: default case is handled by cobra framework
+	// NOTE: Default case is handled by cobra framework
 	switch {
 	case arg == "hook":
-		output = util.ConvertMapKeysToSlice(git.Hooks())
+		out = util.ConvertMapKeysToSlice(git.Hooks())
 	case arg == "lang":
-		output = util.ConvertMapKeysToSlice(git.SupportedLangs())
+		out = util.ConvertMapKeysToSlice(git.SupportedLangs())
 	}
 
-	for _, key := range output {
+	for _, key := range out {
 		cmd.Printf(" %s\n", key)
 	}
 
