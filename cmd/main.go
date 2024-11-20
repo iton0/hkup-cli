28c507e04bd5c0de7f88eac17c6995ee35b6d3c9
Ihsan Tonuzi <115842560+iton0@users.noreply.github.com>
2024-11-20 01:51:47 -0500

    feat: add template subcommand (#24)
    - Created `template` subcommand to manage reusable Git hook templates.
    - Implemented `create`, `copy`, `edit`, and `remove` subcommands for template management.
    - Added the ability to configure Git hook templates for multiple use cases.
    
    feat: add config subcommand
    
    - Created `config` subcommand to manage HkUp configuration settings.
    - Subcommands include `get` and `set` for managing configuration settings.
    - NOTE: This feature is currently hidden and will be available once configuration settings are finalized.
    
    chore: improve install script
    
    - Improved script to handle both installation and update operations when the version is lower than the release version.
    
    chore: update release-please GitHub action
    
    - Moved logic from `scripts/build` to `release-please` action.
    - Removed `scripts/build` to simplify release process.
    
    chore: use defaults for Go dependency checking
    
    - Removed settings for day, time, and timezone from Go dependency checking.
    - Dependabot now uses the default weekly time settings for Go dependencies.
    
    chore: fix pre-commit git hook
    
    - Removed adding all files after running the Go formatter, fixing the issue where unwanted files were being added to commits.
    
    refactor(cmd): simplify flag handling & init logic
    
    - Renamed flags for consistency: `Lang` to `LangFlg`, `GitDir` to `GitDirFlg`, `WorkTree` to `WorkTreeFlg`.
    - Consolidated command registration into `root.go`.
    - Simplified the `init()` function and centralized logic.
    
    refactor(logic): update flags and simplify hook commands
    
    - Simplified Add, Remove, and Init logic by centralizing directory checks.
    - Renamed flags: `Lang` to `LangFlg`, `GitDir` to `GitDirFlg`, `WorkTree` to `WorkTreeFlg`.
    - Improved readability and error handling in file operations.
    
    refactor(git): improve comments and simplify hook logic
    
    - Updated comments for clarity and consistency in the Git package.
    - Simplified `GetHook` and `GetLang` functions to improve readability.
    - Updated the `supportedLangs` map to explicitly include `sh` and `bash`.
    
    feat(util): add file ops, prompts, and config functions
    
    - Added utility functions for terminal prompts: `YesNoPrompt` and `UserInputPrompt`.
    - Expanded file handling functions: `CreateDirectory` and `CopyFile`.
    - Introduced functions to handle HkUp configuration paths: `GetConfigDirPath`, `GetConfigFilePath`, `GetTemplateDirPath`.
    - Added functions for TOML file manipulation: `GetTOMLValue`, `SetTOMLValue`.
    - Implemented `GetEditor` function to determine the default editor based on configuration, git, or environment variables.

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,6 @@
 /*
 Package cmd initializes all commands (including root command) for the HkUp CLI.
-
-Additionally, the package holds all tests for commands.
 */
 package cmd
 
-// Note: This file should be kept empty.
+// NOTE: This file is for documentation purposes and should be kept empty.
--- internal/logic/remove.go
+++ internal/logic/remove.go
@@ -5,32 +5,38 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/iton0/hkup-cli/internal/git"
 	"github.com/iton0/hkup-cli/internal/util"
 	"github.com/spf13/cobra"
 )
 
-// Remove deletes a specified Git hook from the hkup folder.
+// Remove deletes a specified Git hook from the .hkup directory.
 // It takes a single argument, which is the name of the hook to be removed.
 //
-// Returns:
-//   - error: Returns an error if the hkup folder does not exist, if the specified hook is not found,
-//     or if there is an issue deleting the file; otherwise, it returns nil.
+// Returns error if:
+//   - the .hkup directory does not exist
+//   - the specified hook is not found
+//   - issue deleting the file
 func Remove(cmd *cobra.Command, args []string) error {
+	// cannot remove if .hkup directory does not exist
+	if !util.DoesDirectoryExist(util.HkupDirName) {
+		return fmt.Errorf("failed running \"hkup remove\"\n%s directory does not exist", util.HkupDirName)
+	}
+
 	hook := args[0]
 
-	if !util.DoesDirectoryExist(FullPath) {
-		return fmt.Errorf("failed running \"hkup remove\"\n%s folder does not exist", FullPath)
+	// Validates that arg is a supported git hook
+	_, err := git.GetHook(hook)
+	if err != nil {
+		return err
 	}
 
-	filePath := filepath.Join(FullPath, hook)
+	filePath := filepath.Join(util.HkupDirName, hook)
 
+	// Cannot remove if git hook does not exist in the .hkup directory
 	if !util.DoesFileExist(filePath) {
-		return fmt.Errorf("not supported hook: %s", hook)
+		return fmt.Errorf("hook does not exist in current working directory: %s", hook)
 	}
 
-	if err := os.Remove(filePath); err != nil {
-		return fmt.Errorf("failed deleting file: %w", err)
-	}
-
-	return nil
+	return os.Remove(filePath)
 }
