122c915eb2b74511148849b518dd3ab8f0f1885a
iton0 <iton442@gmail.com>
2024-11-19 21:28:52 -0500

    chore: improve install script
    - improved script to either install or update when version is lower than
      release version.
    
    chore: update release-please github action
    
    - deleted the scripts/build script
    - moved the logic from the scripts/build script to the release-please
      action
    
    chore: use defaults for go dependency checking
    
    - removed settings for day, time, and timezone from dependency checking
      for go mod
    
      The dependabot now uses the default weekly time settings.
    
    chore: fix pre-commit git hook
    
    - removed adding all files after running go formatter
    
    This fixes issue of unwanted files/directories added to the commit.
    
    feat: add template subcommand
    
    - created template subcommand for use with git hook templates (reusable
      git hooks)
    - created subcommands for template to create, copy, edit, and remove
      templates.
    
    feat: add config subcommand
    
    - created HkUp configuration settings
    - created subcommands for config to get and set configuration settings
    
    NOTE: this feature is hidden and will be available when configuration
    settings are finalized.
    
    refactor(cmd): simplify flag handling & init logic
    
    - Rename flags for consistency: `Lang` to `LangFlg`, `GitDir` to `GitDirFlg`, and `WorkTree` to `WorkTreeFlg`.
    - Consolidate command registration in `root.go` by removing redundant `rootCmd.AddCommand` calls from individual command files (add.go, remove.go, list.go, doc.go).
    - Simplify `init()` functions in command files to centralize logic.
    - Update root command version handling to dynamically use `version` variable.
    - Clarify documentation comment in `main.go`.
    
    refactor(logic): update flags and simplify hook commands
    
    - Rename flags: `Lang` to `LangFlg`, `GitDir` to `GitDirFlg`, `WorkTree` to `WorkTreeFlg` for consistency.
    - Simplify the Add, Remove, and Init logic by centralizing directory checks and reducing redundant code.
    - Update file handling to improve readability and error handling.
    - Adjust comments for better clarity and consistency across functions.
    
    refactor(git): improve comments and simplify hook logic
    
    - Updated comments for clarity and consistency in the Git package.
    - Removed unnecessary constant `HookDocSite`, and clarified hook documentation URL logic.
    - Simplified the `GetHook` and `GetLang` functions to improve readability and error handling.
    - Updated the `supportedLangs` map to explicitly include `sh` and `bash`, ensuring clearer intent.
    
    feat(util): add file ops, prompts, and config functions
    
    - Added utility functions for terminal prompts, including YesNoPrompt and UserInputPrompt, to improve user interaction.
    - Expanded file handling functions, including CreateDirectory and CopyFile, to support additional use cases.
    - Introduced functions to handle HkUp configuration paths:
      - GetConfigDirPath
      - GetConfigFilePath
      - GetTemplateDirPath
    - Added functions for TOML file manipulation:
      - GetTOMLValue (retrieve a value from a TOML file)
      - SetTOMLValue (set a value in a TOML file)
    - Implemented GetEditor function to determine the default editor for HkUp from configuration, git, or environment variables.
    - Refactored existing functions for clarity and consistency (e.g., CreateFile, DoesDirectoryExist).

---
+++ cmd/template/main.go
@@ -0,0 +1,9 @@
+/*
+Package template initializes the template subcommand and its subcommands.
+
+This package is utilized in the root command of [github.com/iton0/hkup-cli/cmd]
+package.
+*/
+package template
+
+// NOTE: This file is for documentation purposes and should be kept empty.
