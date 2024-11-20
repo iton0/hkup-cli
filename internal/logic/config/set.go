569a205c82ca664032bb50ae32466d262a00868c
iton0 <iton442@gmail.com>
2024-11-19 22:15:08 -0500

    feat: add config subcommand
    - created HkUp configuration settings
    - created subcommands for config to get and set configuration settings
    
    NOTE: this feature is hidden and will be available when configuration
    settings are finalized.

---
+++ internal/logic/config/set.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"github.com/iton0/hkup-cli/internal/util"
+	"github.com/spf13/cobra"
+)
+
+// Set updates a configuration setting with a new value.
+//
+// Returns error if issue with settings the configuration setting.
+func Set(cmd *cobra.Command, args []string) error {
+	// Either setting configuration setting is successful and returns nil or
+	// returns error
+	return util.SetTOMLValue(util.GetConfigFilePath(), args[0], args[1])
+}
